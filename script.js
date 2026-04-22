fb49bb03dea896b07c4707c5e0559cb1dc31faba
Mathilde Tilsted Hansen <eaa25mtha@students.eaaa.dk>
2026-04-22 10:57:36 +0200

    kommentare og omnavngivning af lottie filen

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 "use strict";
 
 //styrer animationerne når tingene skal loades
+//variabler
 let animation = null;
 let progress = 0;
 let isAnimating = false;
 
 function galleryAnimation() {
   animation = lottie.loadAnimation({
+    //kriterier, hvad skal den hentes som, skal der være loop, autoplay og hvor kommer den fra?
     container: document.getElementById("lottie-kasse"),
     renderer: "svg",
     loop: true,
     autoplay: true,
-    path: "lottie/Showcase.json",
+    path: "lottie/foto-gallery.json",
   });
 }
 
+//lytter på galleryAnimation funktionen
 window.addEventListener("load", galleryAnimation);
